a554bd2e3b0cb124ab64f4793bafbffb52d93a2e
Brent Velandra <brent.velandra@gmail.com>
2025-11-10 20:18:42 -0500

    refactor

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class StringCalculator {
-    public static int add(String s) {
-        if(!"".equals(s)){
-            return Integer.valueOf(s);
+    public static int add(String input) {
+        if(!input.isEmpty()){
+            return Integer.valueOf(input);
         }
         return 0;
     }

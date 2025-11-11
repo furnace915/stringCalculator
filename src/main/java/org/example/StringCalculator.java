fa9df13673c0cf36ac33cf9892e1c614195b91d4
Brent Velandra <brent.velandra@gmail.com>
2025-11-10 20:53:04 -0500

    refactor to while loop with iterator

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,16 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class StringCalculator {
     public static int add(String... input) {
+
         int result = 0;
 
-        for (String val : input) {
-            if (val.isEmpty()){
-                result = result + 0;
+        Iterator<String> inputIterator = Arrays.stream(input).iterator();
+        while (inputIterator.hasNext()) {
+            String current = inputIterator.next();
+            if (current.isEmpty()) {
+                result += 0;
+            } else {
+                result += Integer.parseInt(current);
             }
-            else result += Integer.parseInt(val);
         }
-
         return result;
     }
 }
